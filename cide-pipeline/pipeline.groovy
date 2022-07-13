e8372261454e194f1debb88338138afcfe4e17ad
Hitesh Mankar <Hiteshmankar777@gmail.com>
2022-07-13 12:59:20 +0530

    --- Auto Git Commit ---

--- cide-pipeline/pipeline.groovy
+++ cide-pipeline/pipeline.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Pull') { 
             steps {
-                
+              git 'https://github.com/Hiteshmankar123/devops-cicd-demo.git'  
             }
         }
         stage('Build') { 
